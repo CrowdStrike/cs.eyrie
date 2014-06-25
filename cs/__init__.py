477475f9ce8bd99883fbe1a6b5c7dd8628ab9255
David Blewett <davidb@sixfeetup.com>
2014-06-25 14:25:45 -0400

    Switch to true namespace package.

--- cs/__init__.py
+++ cs/__init__.py
@@ -2,7 +2,5 @@
 # Copyright (C) 2014 CrowdStrike, Inc. and contributors
 # This file is subject to the terms and conditions of the BSD License.
 # See the file LICENSE in the main directory for details
-
-__author__ = 'CrowdStrike, Inc.'
-__email__ = 'csoc@crowdstrike.com'
-__version__ = '0.1.0'
+import pkg_resources
+pkg_resources.declare_namespace(__name__)
--- cs/eyrie/__init__.py
+++ cs/eyrie/__init__.py
@@ -2,3 +2,6 @@
 # Copyright (C) 2014 CrowdStrike, Inc. and contributors
 # This file is subject to the terms and conditions of the BSD License.
 # See the file LICENSE in the main directory for details
+__author__ = 'CrowdStrike, Inc.'
+__email__ = 'csoc@crowdstrike.com'
+__version__ = '0.1.0'
