7153bb434a206b56fb0cd14dd08724411b6093b7
David Blewett <david.blewett@crowdstrike.com>
2015-01-24 00:47:29 -0500

    Update router to be a Proxy, to allow monitoring of data flowing through.

--- cs/eyrie/scripts/kafka_router.py
+++ cs/eyrie/scripts/kafka_router.py
@@ -7,14 +7,15 @@ import logging
 from setproctitle import setproctitle
 
 import zmq
-from zmq.devices import ThreadDevice
+from zmq.devices import ThreadProxy
 
 from cs.eyrie.scripts.kafka_consumer import Ranger
 from cs.eyrie.config import setup_logging
 
 
 def main(inbound=Ranger.output.endpoint,
-         outbound='ipc:///tmp/kafka_consume.ipc'):
+         outbound='ipc:///tmp/kafka_consume.ipc',
+         monitor='ipc:///tmp/kafka_monitor.ipc'):
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--config',
                         help='Path to config file',
@@ -25,6 +26,9 @@ def main(inbound=Ranger.output.endpoint,
     parser.add_argument('--consumer-socket',
                         help='Path to socket for downstream consumers',
                         default=outbound)
+    parser.add_argument('--monitor-socket',
+                        help='Path to socket for monitoring',
+                        default=monitor)
     pargs = parser.parse_args()
 
     if pargs.title is not None:
@@ -35,11 +39,12 @@ def main(inbound=Ranger.output.endpoint,
     logger = logging.getLogger('eyrie.kafka_router')
     logger.info('Starting Kafka feed router')
 
-    td = ThreadDevice(zmq.STREAMER, zmq.PULL, zmq.PUSH)
-    td.bind_in(inbound)
-    td.bind_out(outbound)
-    td.start()
-    td.join()
+    tp = ThreadProxy(zmq.PULL, zmq.PUSH, zmq.PUB)
+    tp.bind_in(inbound)
+    tp.bind_out(pargs.consumer_socket)
+    tp.bind_mon(pargs.monitor_socket)
+    tp.start()
+    tp.join()
 
     logger.info('Kafka feed router exiting')
 
