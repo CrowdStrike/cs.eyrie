0ea6752e12e86ca44186addc2b8ac53192499395
Matt DeBoard <matt.deboard@gmail.com>
2016-03-28 15:22:24 -0500

    Increment patch version.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ test_requirements = [
 
 setup(
     name='cs.eyrie',
-    version='0.2',
+    version='0.2.1',
     description='Primitives for building ZMQ pipelines.',
     long_description=readme + '\n\n' + history,
     author='CrowdStrike, Inc.',
