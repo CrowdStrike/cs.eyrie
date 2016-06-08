020f0dcdcb09a9ebd39dd1c49c7c04ecf1630e81
David Blewett <david.blewett@crowdstrike.com>
2016-06-08 07:16:12 -0400

    Bump minor.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ test_requirements = [
 
 setup(
     name='cs.eyrie',
-    version='0.3.0',
+    version='0.4.0',
     description='Primitives for building ZMQ pipelines.',
     long_description=readme + '\n\n' + history,
     author='CrowdStrike, Inc.',
